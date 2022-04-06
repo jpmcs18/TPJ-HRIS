6d4c60bafd9761d0b5c579cce037d82731d4a85f
Raff Julius Martinez <raffjulius.martinez@outlook.com>
2022-04-06 11:26:16 +0800

    Fast load home. manual click for data

--- WebTemplate/Controllers/HomeController.cs
+++ WebTemplate/Controllers/HomeController.cs
@@ -1,14 +1,10 @@
-﻿using ProcessLayer.Processes;
+﻿using ProcessLayer.Helpers;
+using ProcessLayer.Processes;
+using ProcessLayer.Processes.Kiosk;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using P = WebTemplate.Models.Personnel;
-using ProcessLayer.Helpers;
-using System.IO;
-using System.Configuration;
-using ProcessLayer.Processes.Kiosk;
 
 namespace WebTemplate.Controllers
 {
@@ -17,16 +13,6 @@ namespace WebTemplate.Controllers
         // GET: Home
         public ActionResult Index(P.Index model)
         {
-            model.BirthdayCelebrantsRecent = PersonnelProcess.GetBirthdayCelebrantsThisMonthRecent().ToList();
-            model.BirthdayCelebrantsToday = PersonnelProcess.GetBirthdayCelebrantsThisDay().ToList();
-            model.BirthdayCelebrantsUpcoming = PersonnelProcess.GetBirthdayCelebrantsThisMonthUpcoming().ToList();
-
-            model.ExpiringLicensesRecent = PersonnelLicenseProcess.GetExpiringLicensesThisMonthRecent().ToList();
-            model.ExpiringLicensesToday = PersonnelLicenseProcess.GetExpiringLicensesThisDay().ToList();
-            model.ExpiringLicensesUpcoming = PersonnelLicenseProcess.GetExpiringLicensesThisMonthUpcoming().ToList();
-
-            model.KioskNotifications = KioskNotificationProcess.Instance.GetList(User.UserID)?.Where(c => c.Count > 0)?.ToList();
-
             if (Request.IsAjaxRequest())
             {
                 return PartialViewCustom("Index", model);
@@ -34,6 +20,43 @@ namespace WebTemplate.Controllers
             else
             {
                 return ViewCustom("Index", model);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetTab(int tabNo)
+        {
+            try
+            {
+                P.Index model = new();
+                ModelState.Clear();
+                if (tabNo == 1)
+                {
+                    model.BirthdayCelebrantsRecent = PersonnelProcess.GetBirthdayCelebrantsThisMonthRecent().ToList();
+                    model.BirthdayCelebrantsToday = PersonnelProcess.GetBirthdayCelebrantsThisDay().ToList();
+                    model.BirthdayCelebrantsUpcoming = PersonnelProcess.GetBirthdayCelebrantsThisMonthUpcoming().ToList();
+
+                    return PartialView("~/Views/Home/_BirthdayCelebrants.cshtml", model);
+                }
+                if (tabNo == 2)
+                {
+                    model.ExpiringLicensesRecent = PersonnelLicenseProcess.GetExpiringLicensesThisMonthRecent().ToList();
+                    model.ExpiringLicensesToday = PersonnelLicenseProcess.GetExpiringLicensesThisDay().ToList();
+                    model.ExpiringLicensesUpcoming = PersonnelLicenseProcess.GetExpiringLicensesThisMonthUpcoming().ToList();
+
+                    return PartialView("~/Views/Home/_ExpiringLicenses.cshtml", model);
+                }
+                else
+                {
+                    model.KioskNotifications = KioskNotificationProcess.Instance.GetList(User.UserID)?.Where(k => k.Count > 0)?.ToList();
+
+                    return PartialView("~/Views/Home/_KioskNotifications.cshtml", model.KioskNotifications);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { msg = false, res = ex.GetActualMessage() });
             }
         }
     }
